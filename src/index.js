8b61c5947bcab4e8b606021067c5b13914fab03f
dwcarey <danielwilliamcarey@gmail.com>
2023-06-11 16:29:45 +0800

    added prependValue, getSize, getHead, getTail, getbyIndex, popNode, containsValue, getIndexOf, and to string functions and associated tests

--- src/index.js
+++ src/index.js
@@ -18,6 +18,125 @@ class LinkedList {
       currentNode.setNextNode(newNode);
     }
   }
+
+  prependValue(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      const currentNode = this.head;
+      newNode.setNextNode(currentNode);
+      this.head = newNode;
+    }
+  }
+
+  getSize() {
+    if (this.head === null) {
+      return 0;
+    }
+
+    let currentNode = this.head;
+    let sizeCount = 1;
+    while (currentNode.getNextNode() !== null) {
+      sizeCount += 1;
+      currentNode = currentNode.getNextNode();
+    }
+    return sizeCount;
+  }
+
+  getHead() {
+    if (this.head === null) {
+      return "Ain't no nodes";
+    }
+
+    return this.head;
+  }
+
+  getTail() {
+    if (this.head === null) {
+      return "Ain't no nodes";
+    }
+    let currentNode = this.head;
+    while (currentNode.getNextNode() !== null) {
+      currentNode = currentNode.getNextNode();
+    }
+    return currentNode;
+  }
+
+  //starting 0
+  getByIndex(index) {
+    if (this.head === null) {
+      return "Ain't no nodes";
+    }
+
+    let currentNode = this.head;
+    let currentIndex = 0;
+
+    while (currentIndex !== index) {
+      currentNode = currentNode.getNextNode();
+      currentIndex += 1;
+    }
+    return currentNode;
+  }
+
+  //get size
+  //getbyindex size - 2
+  //should be 2nd last node
+  //set nextnode to null
+
+  popNode() {
+    if (this.head === null) {
+      return "Ain't no nodes";
+    }
+    const length = this.getSize();
+    const secondLastNode = this.getByIndex(length - 2);
+    return secondLastNode.setNextNode(null);
+  }
+
+  containsValue(value) {
+    if (this.head === null) {
+      return "Ain't no nodes";
+    }
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.getValue() === value) {
+        return true;
+      }
+      currentNode = currentNode.getNextNode();
+    }
+    return false;
+  }
+
+  getIndexOf(value) {
+    if (this.head === null) {
+      return "Ain't no nodes";
+    }
+    let currentNode = this.head;
+    let currentIndex = 0;
+    while (currentNode !== null) {
+      if (currentNode.getValue() === value) {
+        return currentIndex;
+      }
+      currentIndex += 1;
+      currentNode = currentNode.getNextNode();
+    }
+    return null;
+  }
+
+  toString() {
+    if (this.head === null) {
+      return "Ain't no nodes";
+    }
+    let currentNode = this.head;
+    let listString = '';
+
+    while (currentNode.getNextNode() !== null) {
+      listString += `( ${currentNode.getValue()} ) => `;
+      currentNode = currentNode.getNextNode();
+    }
+    return listString;
+  }
+
   // other functions
 }
 
@@ -25,10 +144,31 @@ const linkedListOne = new LinkedList();
 console.log(linkedListOne);
 
 linkedListOne.appendValue('X');
-console.log(linkedListOne);
-
 linkedListOne.appendValue('Y');
-console.log(linkedListOne);
-
 linkedListOne.appendValue('Z');
-console.log(linkedListOne);
+linkedListOne.prependValue('A');
+linkedListOne.prependValue('B');
+linkedListOne.prependValue('C');
+
+console.log(linkedListOne.getSize());
+
+console.log(linkedListOne.getHead());
+
+console.log(linkedListOne.getTail());
+
+console.log(linkedListOne.getByIndex(0));
+console.log(linkedListOne.getByIndex(4));
+
+console.log(linkedListOne.popNode());
+
+console.log(linkedListOne.containsValue('A')); //true
+console.log(linkedListOne.containsValue('B')); //true
+console.log(linkedListOne.containsValue('H')); //false
+console.log(linkedListOne.containsValue(1)); //false
+
+console.log(linkedListOne.getIndexOf('A')); //2
+console.log(linkedListOne.getIndexOf('B')); //1
+console.log(linkedListOne.getIndexOf('H')); //null
+console.log(linkedListOne.getIndexOf(1)); //null
+
+console.log(linkedListOne.toString());
